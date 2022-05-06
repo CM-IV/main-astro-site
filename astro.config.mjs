ec6e7526db8a0ea5a17aa99b2181ac63ca66e903
Charlie Mathis IV <charlie@debian-BULLSEYE-live-builder-AMD64>
2022-05-06 10:23:39 -0500

    Using env var for astro config site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  site: import.meta.env.SITE
+  site: import.meta.site
 });
