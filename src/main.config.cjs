f6fea104fb3633bcb5f65d04419559a721aaba4f
CM-IV <cmathIV@protonmail.com>
2022-07-04 15:10:13 -0500

    Added Education section with email

--- src/main.config.cjs
+++ src/main.config.cjs
@@ -6,6 +6,8 @@ export default {
     socialMedia: {
       github: "CM-IV",
       discordServer: "erE3jrtpTb",
+      ua_little_rock: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5361&returnto=2423",
+      email: "chuck@civdev.xyz"
     },
     pageSize: 6,
     i18n: {
