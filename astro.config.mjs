5bfcc0c15024992cde1a3993bd7031c44f499190
CM-IV <cmathIV@protonmail.com>
2022-07-28 04:21:29 -0500

    Updated astro to v1.0.0-rc.1, added download page for image optimizer app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [image()],
   site: import.meta.site,
   markdown: {
-    remarkPlugins: [
-      "remark-gfm",
-      "remark-smartypants",
-      "remark-math"
-    ],
-    rehypePlugins: [
-      "rehype-katex"
-    ]
+    remarkPlugins: ["remark-gfm", "remark-smartypants", "remark-math"],
+    rehypePlugins: ["rehype-katex"]
+  },
+  legacy: {
+    astroFlavoredMarkdown: true
   }
 });
--- src/main.config.cjs
+++ src/main.config.cjs
@@ -6,7 +6,8 @@ export default {
     socialMedia: {
       github: "CM-IV",
       discordServer: "erE3jrtpTb",
-      ua_little_rock: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5361&returnto=2423",
+      ua_little_rock1: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5361&returnto=2423",
+      ua_little_rock2: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5306&returnto=2443",
       email: "chuck[at]civdev[dot]xyz"
     },
     pageSize: 8,
