8efad3e48932e0059f20a85b38b3caf5faa2ad24
CM-IV <cmathIV@protonmail.com>
2022-08-04 21:15:18 -0500

    Updated image optimizer

--- src/main.config.cjs
+++ src/main.config.cjs
@@ -8,7 +8,9 @@ export default {
       discordServer: "erE3jrtpTb",
       ua_little_rock1: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5361&returnto=2423",
       ua_little_rock2: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5306&returnto=2443",
-      email: "chuck[at]civdev[dot]xyz"
+      email: "chuck[at]civdev[dot]xyz",
+      linuxFileUrl: "https://api.anonfiles.com/v2/file/S14fqd29y2/info",
+      winFileUrl: "https://api.anonfiles.com/v2/file/L141q32fy7/info"
     },
     pageSize: 8,
     i18n: {
