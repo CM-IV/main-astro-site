93cf42c477c706a834ff510bcb0ed761ad9abe5f
CM-IV <cmathIV@protonmail.com>
2022-08-04 21:31:38 -0500

    Fix file urls

--- src/main.config.cjs
+++ src/main.config.cjs
@@ -9,8 +9,8 @@ export default {
       ua_little_rock1: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5361&returnto=2423",
       ua_little_rock2: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5306&returnto=2443",
       email: "chuck[at]civdev[dot]xyz",
-      linuxFileUrl: "https://api.anonfiles.com/v2/file/S14fqd29y2/info",
-      winFileUrl: "https://api.anonfiles.com/v2/file/L141q32fy7/info"
+      linuxFileUrl: "https://api.anonfiles.com/v2/file/hfA5qc22y3/info",
+      winFileUrl: "https://api.anonfiles.com/v2/file/k2A8q52ey7/info"
     },
     pageSize: 8,
     i18n: {
