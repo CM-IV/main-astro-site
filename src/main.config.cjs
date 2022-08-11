b41d98bae9ff60d6aaf7bfd5be34060d854fd817
CM-IV <cmathIV@protonmail.com>
2022-08-11 10:08:38 -0500

    Fix download links

--- src/main.config.cjs
+++ src/main.config.cjs
@@ -9,8 +9,8 @@ export default {
       ua_little_rock1: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5361&returnto=2423",
       ua_little_rock2: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5306&returnto=2443",
       email: "chuck[at]civdev[dot]xyz",
-      linuxFileUrl: "https://api.anonfiles.com/v2/file/hfA5qc22y3/info",
-      winFileUrl: "https://api.anonfiles.com/v2/file/k2A8q52ey7/info"
+      linuxFileUrl: "https://www.mediafire.com/file/am5hj3oqkmknxch/Web_Image_Optimizer.AppImage/file",
+      winFileUrl: "https://www.mediafire.com/file/fku9txzy23n7jb8/Web_Image_Optimizer_Setup.exe/file"
     },
     pageSize: 8,
     i18n: {
