1f6fdc8cf75e185787a6d75a09c2020520c2dacd
CM-IV <cmathIV@protonmail.com>
2022-08-26 16:01:28 -0500

    Updated Astro to v1.1.0, using mdx now instead of markdown, updated astro config, moved dependencies to devDependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image()],
+  integrations: [image(), mdx({
+    remarkPlugins: { extends: [remarkMath] },
+    rehypePlugins: [rehypeKatex]
+  })],
   site: import.meta.site,
-  markdown: {
-    remarkPlugins: ["remark-gfm", "remark-smartypants", "remark-math"],
-    rehypePlugins: ["rehype-katex"]
-  },
-  legacy: {
-    astroFlavoredMarkdown: true
-  }
+  // markdown: {
+  //   remarkPlugins: ["remark-gfm", "remark-smartypants", "remark-math"],
+  //   rehypePlugins: ["rehype-katex"]
+  // }
 });
--- src/pages/news/rss.xml.js
+++ src/pages/news/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss";
 
-const allPosts = import.meta.globEager('../posts/*.md');
+const allPosts = import.meta.globEager('../posts/*.mdx');
 
 const posts = Object.values(allPosts);
 
@@ -9,8 +9,8 @@ posts.sort((a, b) => (a.frontmatter.id) - (b.frontmatter.id));
 const sortedPosts = posts.reverse();
 
 export const get = () => rss({
-    title: "Occident Tech Software News",
-    description: "Latest web development news",
+    title: "Dreamland Software News",
+    description: "Curated web development and security news.",
     site: import.meta.env.PUBLIC_SITE,
     // stylesheet: '/rss/styles.xsl',
     items: sortedPosts.map(item => ({
