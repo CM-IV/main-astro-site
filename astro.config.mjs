f0bc03874911d1e40982211ec8683c626a661f6f
CM-IV <cmathIV@protonmail.com>
2022-09-30 10:35:49 -0500

    Uninstall @astrojs/image, updated mdx and astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), mdx({
+  integrations: [mdx({
     rehypePlugins: [rehypeKatex],
     remarkPlugins: [remarkMath],
     extendPlugins: 'astroDefaults'
