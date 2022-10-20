9f3c9b778ffd9cfa4a6775a9987dc51360c6fe17
CM-IV <cmathIV@protonmail.com>
2022-10-20 09:08:58 -0500

    Added modal for analytics tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx({
-    rehypePlugins: [rehypeKatex],
-    remarkPlugins: [remarkMath],
-    extendPlugins: 'astroDefaults'
-  })],
+  integrations: [preact(),
+    mdx({
+      rehypePlugins: [rehypeKatex],
+      remarkPlugins: [remarkMath],
+      extendPlugins: 'astroDefaults'
+    })
+  ],
   site: import.meta.site,
 });
---
+++ src/components/animated/modal.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "preact/hooks";
+import { Fragment } from "preact";
+
+const Modal = () => {
+    const [showModal, setShowModal] = useState(true);
+
+    const resetModal = () => {
+        setShowModal(false);
+    }
+
+    useEffect(() => {
+        const stateData = window.sessionStorage.getItem("MODAL_STATE");
+        if (stateData) {
+            setShowModal(JSON.parse(stateData));
+        }
+    }, [])
+
+    useEffect(() => {
+        window.sessionStorage.setItem("MODAL_STATE", JSON.stringify(showModal));
+    }, [showModal])
+
+    return (
+        <Fragment>
+            {showModal ? (
+                <section onClick={resetModal}>
+                    <div class="terminal-alert terminal-alert-error">
+                        This website has one javascript tracker completely independant 
+                        of Google Analytics.  Please consider disabling your
+                        ad blocker for this site.
+                    </div>
+                </section>
+            ) : (null)}
+        </Fragment>
+    )
+}
+
+export { Modal };
