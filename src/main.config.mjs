84d960ab134b8349bbcc651f7f2d1df85787a3e3
CM-IV <cmathIV@protonmail.com>
2022-10-26 14:35:44 -0500

    Updated website for YT Links

--- src/main.config.mjs
+++ src/main.config.mjs
@@ -5,9 +5,6 @@ export default {
     dateFormat: "dd.MM.yyyy HH:mm",
     socialMedia: {
       github: "CM-IV",
-      discordServer: "erE3jrtpTb",
-      ua_little_rock1: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5361&returnto=2423",
-      ua_little_rock2: "https://catalog.ualr.edu/preview_program.php?catoid=20&poid=5306&returnto=2443",
       email: "chuck[at]civdev[dot]xyz",
       linuxFileUrl: "https://www.mediafire.com/file/ovm6hgotd362ssb/Web+Image+Optimizer.AppImage/file",
       winFileUrl: "https://www.mediafire.com/file/xbj0gxeujv68flb/Web+Image+Optimizer-Windows-Setup.exe/file"
