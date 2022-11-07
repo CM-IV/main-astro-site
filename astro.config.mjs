1526542333ddcf8782b16fdcc03fc41f9b1026d0
CM-IV <cmathIV@protonmail.com>
2022-11-07 11:56:21 -0600

    Added sitemaps for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,18 @@ import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(),
+  site: 'https://home.civdev.xyz' || import.meta.env.site,
+  integrations: [
+    sitemap(),
+    preact(),
     mdx({
       rehypePlugins: [rehypeKatex],
       remarkPlugins: [remarkMath],
       extendPlugins: 'astroDefaults'
     })
   ],
-  site: import.meta.site,
 });
