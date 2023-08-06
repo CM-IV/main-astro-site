4695c6972ecc9bb049b1fe3c31010c21d816b03b
CM-IV <cmathIV@protonmail.com>
2023-08-06 10:57:20 -0500

    Added rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function get(context) {
+	const posts = await getCollection('blog');
+	return rss({
+		title: "Oceanus Group",
+		description: "Oceanus Group - innovation and success for your business.",
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			description: post.data.snippet,
+			pubDate: post.data.publishDate,
+			link: `/blog/${post.slug}/`,
+		})),
+	});
+
+}
