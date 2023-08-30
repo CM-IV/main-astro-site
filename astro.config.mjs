c3f71999fbae41dc271a9e3f17afa80aff07b99b
CM-IV <cmathIV@protonmail.com>
2023-08-30 10:25:30 -0500

    Update Astro to v3.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true
-  },
   markdown: {
     shikiConfig: {
       theme: 'dracula',
