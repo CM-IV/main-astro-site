fba76f66f21b7931a3d6f795193452c2440f39ae
CM-IV <cmathIV@protonmail.com>
2023-08-30 10:55:21 -0500

    Remove @astro/images integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
@@ -16,9 +15,6 @@ export default defineConfig({
   site: "https://home.civdev.xyz",
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     mdx(),
     sitemap(),
   ],
