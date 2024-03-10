174638aa024b56d1e72812c5173fb4ba487d292c
CM-IV <cmathIV@protonmail.com>
2024-03-10 12:32:08 -0500

    Remove astro-icon dependency, manually use svg code and re-export as astro component

--- src/components/ui/icons/index.js
+++ src/components/ui/icons/index.js
@@ -1,2 +1,5 @@
 export { default as Tick } from "./tick.astro";
 export { default as Vite } from "./vite.astro";
+export { default as Rust } from "./rust.astro";
+export { default as Node } from "./node.astro";
+export { default as React } from "./react.astro";
